b363b319c80593fd7cb283696c01010efb7277f3
Jhojan Lopez <jhojanlop2@gmail.com>
2023-10-25 22:11:26 -0500

    first commit

---
+++ src/main/java/com/equipotres/medioambiente/MedioambienteApplication.java
@@ -0,0 +1,13 @@
+package com.equipotres.medioambiente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedioambienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedioambienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipotres/medioambiente/MedioambienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipotres.medioambiente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedioambienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
