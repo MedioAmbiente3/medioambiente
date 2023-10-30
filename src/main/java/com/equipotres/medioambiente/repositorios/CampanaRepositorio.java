7ca5f70a96615418cc8bd2baefe82e99580f019c
David Alejandro Lugo Gámez <=>
2023-10-30 01:19:12 -0400

    Creado repositorios Usuario, Campana

---
+++ src/main/java/com/equipotres/medioambiente/repositorios/CampanaRepositorio.java
@@ -0,0 +1,10 @@
+package com.equipotres.medioambiente.repositorios;
+import com.equipotres.medioambiente.Entidades.Campana;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CampanaRepositorio extends JpaRepository<Camara,String> {
+/*No se define ningún método personalizado en este repositorio porque
+todas las operaciones necesarias (crear, modificar, listar y eliminar campañas)
+ya están cubiertas por los métodos proporcionados por JpaRepository*/
+}
