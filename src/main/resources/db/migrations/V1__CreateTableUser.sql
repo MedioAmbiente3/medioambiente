953bdbd228ca28a3b75507077adad77aa85d2b00
Adan Slayer <adpa_suru@hotmail.com>
2023-11-03 18:38:15 -0600

    se crea directorio bd

---
+++ src/main/resources/db/migrations/V1__CreateTableUser.sql
@@ -0,0 +1,6 @@
+CREATE TABLE usuario(
+id_usuario CHAR(36) PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    correo TEXT NOT NULL,
+    password TEXT NOT NULL
+);
---
+++ src/main/resources/db/migrations/V2__CreateTableCampaign.sql
@@ -0,0 +1,6 @@
+CREATE TABLE campana(
+id_campana CHAR(36) PRIMARY KEY,
+    titulo TEXT NOT NULL,
+    redaccion TEXT NOT NULL,
+    fecha DATE NOT NULL
+);
