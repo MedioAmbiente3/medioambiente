7135409a533853a5d657956ac27719db5b7c5292
David Alejandro Lugo Gámez <=>
2023-11-17 00:20:50 -0400

    corregida observacion

--- src/main/java/com/equipotres/medioambiente/Repositorios/RolRepositorio.java
+++ src/main/java/com/equipotres/medioambiente/Repositorios/RolRepositorio.java
@@ -12,5 +12,5 @@ import java.util.Optional;
 public interface RolRepositorio extends JpaRepository<Rol, String> {
 
     @Query("SELECT r FROM Rol r WHERE r.nombre = ?1")
-    Optional<Rol> findByNombre(RolEnum rolEnum);
+    Optional<Rol> findByNombre(String nombre);
 }
